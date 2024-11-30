d23d14ba47529900d6a7e7aa9df867746fa7c057
VP45 <pariawalaveer@gmail.com>
2024-11-30 16:38:27 +0530

    eslint ignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    rewrites: async () => {
-        return[
-            {
-                source: '/api/:path*',
-                destination: 'http://localhost:3000/api/:path*'
-            }
-        ]
-    }
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3000/api/:path*",
+      },
+    ];
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
